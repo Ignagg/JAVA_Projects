0b84d373c2a970470653cf4ecfabedf9aa88c91d
Ignacio Jose Paste Kvapil <ignapaste@gmail.com>
2025-05-21 22:34:56 -0300

    modificación del nombre

--- LectorDeTxt.java
+++ LectorDeTxt.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class lector_de_archivos_txt {
+public class LectorDeTxt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce la ruta del archivo .txt: ");
